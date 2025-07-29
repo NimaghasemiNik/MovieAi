c6905b138ce738c94567bc4d55db5e0b2efcec89
NimaNik <nimaghaseminik00@gmail.com>
2025-07-29 17:43:29 +0800

    folder structuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-
+import NavBar from './components/NavBar/NavBar'
 const App = () => {
   return (
-    <div>
-      <main>
+    <div className="root">
+    <NavBar />
+      <main className="content">
+        <div className="toolbar" />
         <Routes>
           <Route path="/movie/:id" element={<h1>Movie Information</h1>} />
           <Route path="/actors/:id" element={<h1>Actors</h1>} />
